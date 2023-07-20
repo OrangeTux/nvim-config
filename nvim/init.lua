622eac72214391954967915efdf9956b6ce7f993
Auke Oosterhoff <auke@orangetux.nl>
2023-07-20 21:50:27 +0200

    Not sure why I made these changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -224,12 +224,9 @@ require('rust-tools').setup({
 	},
 	server = {
 		settings = {
-			on_attach = function(_, bufnr)
-				-- Hover actions
-				vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-				-- Code action groups
-				vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-			end,
+			--on_attach = function(_, bufnr)
+			--vim.keymap.set("n", "<Leader>mc", require('rust-tools').expand_macro.expand_macro())
+			--end,
 			["rust-analyzer"] = {
 				checkOnSave = {
 					command = "clippy"
